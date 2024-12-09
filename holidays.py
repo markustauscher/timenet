708f70ab09da5092b6f901b39e19ef325bf888a5
Markus <mtauscher@googlemail.com>
2024-12-09 10:54:57 +0100

    Add 2025 public and remaining private 2024 holiday and first week of 2025 as holiday

--- holidays.py
+++ holidays.py
@@ -21,6 +21,22 @@ class holidays:
         self.add_holiday("Spanish Constitution Day", date(2024, 12, 6))
         self.add_holiday("Nativity of the Lord", date(2024, 12, 25))
 
+        # 2025
+        self.add_holiday("New Year's Day", date(2025, 1, 1))
+        self.add_holiday("Epiphany", date(2025, 1, 6))
+        self.add_holiday("Holy Thursday", date(2025, 4, 17))
+        self.add_holiday("Good Friday", date(2025, 4, 18))
+        self.add_holiday("Worker's Day", date(2025, 5, 1))
+        self.add_holiday("Festival of the Community of Madrid", date(2025, 5, 2))  # Confirmed, no changes
+        self.add_holiday("San Isidro", date(2025, 5, 15))  # Festival of San Isidro
+        self.add_holiday("Santiago Apóstol", date(2025, 7, 25))
+        self.add_holiday("Assumption of the Virgin", date(2025, 8, 15))
+        self.add_holiday("National Holiday of Spain", date(2025, 10, 12))
+        self.add_holiday("All Saints' Day", date(2025, 11, 1))
+        self.add_holiday("Spanish Constitution Day", date(2025, 12, 6))
+        self.add_holiday("Spanish Constitution Day", date(2025, 12, 8))
+        self.add_holiday("Nativity of the Lord", date(2025, 12, 25))
+
 
         # Add private holidays
         
@@ -52,6 +68,14 @@ class holidays:
         self.add_holiday("Private Holiday", date(2024, 8, 4))
         self.add_holiday("Private Holiday", date(2024, 8, 5))
         self.add_holiday("Private Holiday", date(2024, 8, 6))
+
+        self.add_holiday("Private Holiday", date(2024, 12, 19))
+        self.add_holiday("Private Holiday", date(2024, 12, 24))
+
+        self.add_holiday("Private Holiday", date(2025, 1, 7))
+        self.add_holiday("Private Holiday", date(2025, 1, 8))
+        self.add_holiday("Private Holiday", date(2025, 1, 9))
+        self.add_holiday("Private Holiday", date(2025, 1, 10))
 
 
     def add_holiday(self, name, date):
